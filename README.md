4959d2cea50ad87a1c16830c07e101bc1158f30e
abhaya <gautamabhaya98@gmail.com>
2025-11-16 20:19:46 +0545

    removed old code to start fresh

---
+++ README.md
@@ -0,0 +1,3 @@
+# OAuth 2.0 Integration for Xero API using Django
+
+- Will be updating readme soon.
--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', homepage, name='home'),
-    path('login/', login, name='login'),
-    path('callback/', oauth_callback, name='callback'),
-    path('logout/', logout, name='logout')
-]
